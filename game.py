ea839444da835805dac6c54409972c2ed6f8143c
tshnkuts021 <tshnkuts021@student.wethinkcode.co.za>
2023-02-14 10:11:45 +0200

    completed

--- game.py
+++ game.py
@@ -10,10 +10,9 @@ def number_game():
     secret_number = random.randint(1,100)
     print("Thanks, " + player_name + ". Please enter any number between 1 and 100. Remember, you have only 5 attempts")
 
-    # print("Total number of chances is 5")
+    
     number = random.randint(0,100)
-    print(number)
-    for i in range (4, -1,-1,):
+    for i in range (4,-1,-1):
         x = int(input("Guess the number between 0 to 100: "))
         if x > number:
             print("Your guess is above the number, please guess again")
@@ -30,27 +29,3 @@ def number_game():
 
 if __name__ == "__main__":
     number_game()
-
-
-#    print("*********************************")
-#     print("             WELCOME")
-#     print("    The Number Guessing Game")
-#     print("*********************************")
-#     import random
-#     print("Total chances 3")
-#     num = random.randint(0,100)
-#     print(num)
-#     for i in range (2,-1,-1):
-#         x = int(input("\nGuess the number between 0 to 100: "))
-#         if x > num:
-#             print("Fortunately, you guessed it bigger")
-#             print("Number of chances left: ",i)
-#         elif x == num:
-#             print("Yep, you guess it correct. Congo!!")
-#             print("Number of chances left: ",i)
-#             break
-#         else:
-#             print("Nope, you guessed it smaller")
-#             print("Number of chances left: ",i)
-#     if x!=num:
-#         print("The Secret Number is: ",num)
